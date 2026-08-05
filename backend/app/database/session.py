59c6e03ee5fffb504013c647f74ef0de273f9863
LuisJose2907 <luis.santiago110207@gmail.com>
2026-08-05 01:53:49 -0500

    fix(session): a problem with the package

--- backend/app/database/session.py
+++ backend/app/database/session.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from app.core.seetings import Settings
+from app.core.settings import settings
 from sqlalchemy.orm import sessionmaker
 from collections.abc import Generator
 
